fe906b51142332a519259acf95916302a7c49570
Nguyen Thanh <Thanh.CEAA.K58.HUST@gmail.com>
2021-06-03 21:25:45 +0700

    Add register test (register_test.py) query test (database_test.py) files

---
+++ database_test.py
@@ -0,0 +1,77 @@
+import os
+import time
+import sqlite3
+import numpy as np
+from fingerprint import fingerprint_file, my_spectrogram, find_peaks, idxs_to_tf_pairs, file_to_spectrogram, read_audio_file, fingerprint_audio
+from recognize import recognize_song, get_matches, best_match, get_info_for_song_id
+import uuid
+
+conn = sqlite3.connect("music.sqlite")
+cursor = conn.cursor()
+
+#cursor.execute("SELECT * FROM hash")
+#records = cursor.fetchall()
+#cursor.close()
+#
+#for record in records:
+#    print(record)
+#
+#print("===============================================================================")
+#
+#cursor = conn.cursor()
+#cursor.execute("SELECT * FROM song_info")
+#records = cursor.fetchall()
+#cursor.close()
+#
+#for record in records:
+#    print(record)
+
+#song = recognize_song(filename=r"D:\1NamNguyenTheMinh.mp3",
+#                       db_path="music.sqlite", sample_rate=44100,
+#                       fft_window_size=0.2, peak_box_size=30, point_efficiency=0.8, target_t=1.8,
+#                       target_f=4000, target_start=0.05, threshold=5)
+audio, sr = read_audio_file(audio_path=r"D:\1NamNguyenTheMinh.mp3", sr_desired=44100)
+print(np.min(audio), np.max(audio))
+audio += 0.15 * np.random.randn(audio.shape[0])
+start = time.time()
+hashes = fingerprint_audio(audio, sample_rate=44100,
+                         fft_window_size=0.2, peak_box_size=30, point_efficiency=0.8, target_t=1.8,
+                         target_f=4000, target_start=0.05)
+matches = get_matches(hashes=hashes, db_path="music.sqlite", threshold=5)
+matched_song = best_match(matches=matches)
+song = get_info_for_song_id(song_id=matched_song, db_path="music.sqlite")
+end = time.time()
+print(song, end - start)
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM hash WHERE song_id = ?", (str(uuid.uuid5(uuid.NAMESPACE_OID, os.path.basename(r"D:\Music\Han\Advice.mp3")).int),))
+song_hashes = cursor.fetchall()
+song_hashes.sort(key=lambda x: x[1])
+cursor.close()
+print(len(song_hashes))
+
+hashes = fingerprint_file(filename=r"D:\Music\Han\Advice.mp3", sample_rate=44100, fft_window_size=0.2, peak_box_size=30,
+                            point_efficiency=0.8, target_t=1.8, target_f=4000, target_start=0.05)
+hashes.sort(key=lambda x: x[1])
+print(len(hashes))
+#hashes_1 = fingerprint_file(filename=r"D:\Music\Han\Advice.mp3", sample_rate=44100, fft_window_size=0.2, peak_box_size=20,
+#                            point_efficiency=0.8, target_t=1.8, target_f=4000, target_start=0.05)
+#hashes_2 = fingerprint_file(filename=r"D:\Advice.mp3", sample_rate=44100, fft_window_size=0.2, peak_box_size=20,
+#                            point_efficiency=0.8, target_t=1.8, target_f=4000, target_start=0.05)
+#print("hashes_1: {}".format(hashes_1))
+#print("hashes_2: {}".format(hashes_2))
+
+#f, t, Sxx = file_to_spectrogram(filename=r"D:\Music\Han\Advice.mp3", sample_rate=44100, fft_window_size=0.2)
+#peaks = find_peaks(Sxx=Sxx, peak_box_size=20, point_efficiency=0.8)
+#peaks.sort(key=lambda x: x[1])
+##peaks = idxs_to_tf_pairs(idxs=peaks, t=t, f=f)
+#
+#print("peaks_1: {}".format(peaks))
+
+
+#f, t, Sxx = file_to_spectrogram(filename=r"D:\Advice.mp3", sample_rate=44100, fft_window_size=0.2)
+#peaks = find_peaks(Sxx=Sxx, peak_box_size=20, point_efficiency=0.8)
+#peaks.sort(key=lambda x: x[1])
+##peaks = idxs_to_tf_pairs(idxs=peaks, t=t, f=f)
+#
+#print("peaks_2: {}".format(peaks))
